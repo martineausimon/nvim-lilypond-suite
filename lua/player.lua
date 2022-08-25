f45fa3d71de3238210fb94aa4b64164f3868b501
martineausimon <martineau.simon@gmail.com>
2022-08-25 21:10:54 +0200

    lua/player: improve mappings

--- lua/player.lua
+++ lua/player.lua
@@ -1,6 +1,5 @@
 local lilyPopup = require("nui.popup")
 local event = require("nui.utils.autocmd").event
-local key = vim.api.nvim_buf_set_keymap
 
 local lilyPlayer = lilyPopup({
   enter = true,
@@ -27,49 +26,29 @@ local lilyPlayer = lilyPopup({
 
 lilyPlayer:mount()
 
-lilyPlayer:on({ event.BufWinLeave }, function()
-  vim.schedule(function()
-      popup:unmount()
-    end)
-end)
-
 vim.api.nvim_buf_call(lilyPlayer.bufnr, function() 
   vim.fn.execute("term mpv --loop --config-dir=/tmp/ " .. 
     vim.g.lilyAudioFile
   )
 end)
 
-key(lilyPlayer.bufnr, 'n', 'q',
-  "<cmd>bw!<cr>", {noremap = true})
-key(lilyPlayer.bufnr, 't', 'q',
-  "<cmd>bw!<cr>", {noremap = true})
-key(lilyPlayer.bufnr, 't', '<A-Space>',
-  "<cmd>stopinsert<cr><C-w>w", {noremap = true})
-key(lilyPlayer.bufnr, 'n', '<A-Space>',
-  "<C-w>w", {noremap = true})
-key(lilyPlayer.bufnr, 't', '<Esc>',
-  "<cmd>stopinsert<cr>", {noremap = true})
-key(lilyPlayer.bufnr, 'n', 'h',
-  "i<Left><cmd>stopinsert<cr>", {noremap = true})
-key(lilyPlayer.bufnr, 'n', 'l',
-  "i<Right><cmd>stopinsert<cr>", {noremap = true})
-key(lilyPlayer.bufnr, 'n', '<S-l>',
-  "i<S-Right><cmd>stopinsert<cr>", {noremap = true})
-key(lilyPlayer.bufnr, 'n', '<S-h>',
-  "i<S-Left><cmd>stopinsert<cr>", {noremap = true})
-key(lilyPlayer.bufnr, 'n', 'p',
-  "ip<cmd>stopinsert<cr>", {noremap = true})
-key(lilyPlayer.bufnr, 'n', '{',
-  "i{<cmd>stopinsert<cr>", {noremap = true})
-key(lilyPlayer.bufnr, 'n', '}',
-  "i}<cmd>stopinsert<cr>", {noremap = true})
-key(lilyPlayer.bufnr, 'n', 'j',
-  "i[<cmd>stopinsert<cr>", {noremap = true})
-key(lilyPlayer.bufnr, 'n', 'k',
-  "i]<cmd>stopinsert<cr>", {noremap = true})
-key(lilyPlayer.bufnr, 'n', '<A-l>',
-  "il<cmd>stopinsert<cr>", {noremap = true})
-key(lilyPlayer.bufnr, 'n', ']',
-  "i]<cmd>stopinsert<cr>", {noremap = true})
-key(lilyPlayer.bufnr, 'n', '[',
-  "i[<cmd>stopinsert<cr>", {noremap = true})
+local nrm = { noremap = true }
+
+lilyPlayer:map('n', '<Esc>', function() lilyPlayer:unmount() end, nrm)
+lilyPlayer:map('n', 'q',     function() lilyPlayer:unmount() end, nrm)
+lilyPlayer:map('t', 'q',     function() lilyPlayer:unmount() end, nrm)
+lilyPlayer:map('t', '<A-Space>', "<cmd>stopinsert<cr><C-w>w",     nrm)
+lilyPlayer:map('n', '<A-Space>', "<cmd>stopinsert<cr><C-w>w",     nrm)
+lilyPlayer:map('n', 'h',         "i<Left><cmd>stopinsert<cr>",    nrm)
+lilyPlayer:map('n', 'l',         "i<Right><cmd>stopinsert<cr>",   nrm)
+lilyPlayer:map('n', '<S-l>',     "i<S-Right><cmd>stopinsert<cr>", nrm)
+lilyPlayer:map('n', '<S-h>',     "i<S-Left><cmd>stopinsert<cr>",  nrm)
+lilyPlayer:map('n', 'p',         "ip<cmd>stopinsert<cr>",         nrm)
+lilyPlayer:map('n', '{',         "i{<cmd>stopinsert<cr>",         nrm)
+lilyPlayer:map('n', '}',         "i}<cmd>stopinsert<cr>",         nrm)
+lilyPlayer:map('n', 'j',         "i[<cmd>stopinsert<cr>",         nrm)
+lilyPlayer:map('n', 'k',         "i]<cmd>stopinsert<cr>",         nrm)
+lilyPlayer:map('n', '<A-l>',     "il<cmd>stopinsert<cr>",         nrm)
+lilyPlayer:map('n', ']',         "i]<cmd>stopinsert<cr>",         nrm)
+lilyPlayer:map('n', '[',         "i[<cmd>stopinsert<cr>",         nrm)
+lilyPlayer:map('n', ':',         "",                              nrm)
