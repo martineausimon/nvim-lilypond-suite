2791c292d6550c6631f4220e4755797388af591d
martineausimon <martineau.simon@gmail.com>
2022-09-20 11:02:17 +0200

    ftdetect/filetype.lua: set ft=lua for .lilyrc files

--- ftdetect/filetype.lua
+++ ftdetect/filetype.lua
@@ -5,6 +5,9 @@ if vim.g.do_filetype_lua == 1 then
       ily = 'lilypond',
     },
   })
+  vim.filetype.add({
+    pattern = { ['.lilyrc'] = 'lua' }
+  })
   else
   vim.api.nvim_create_autocmd(
     { 'BufNewFile', 'BufRead' },
@@ -13,6 +16,15 @@ if vim.g.do_filetype_lua == 1 then
       pattern = { '*.ly', '*.ily' },
       group =  vim.api.nvim_create_augroup(
         'lilyFtdetect', { clear = true}) 
-    }
-  )
+  })
+  vim.api.nvim_create_autocmd(
+    { 'BufNewFile', 'BufRead' },
+    {
+      command = 'set ft=lua',
+      pattern = { '.lilyrc' },
+      group =  vim.api.nvim_create_augroup(
+        'lilyrcFtdetect', { clear = true}) 
+  })
 end
+
+
