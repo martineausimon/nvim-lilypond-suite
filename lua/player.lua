dce14662dd1dfcf593d415734f3168ca9d775ff0
martineausimon <martineau.simon@gmail.com>
2022-09-28 23:59:49 +0200

    lua/player.lua: refactor mappings & adding settings for winhighlight

--- lua/player.lua
+++ lua/player.lua
@@ -1,26 +1,28 @@
 local lilyPopup = require("nui.popup")
+local g = vim.g
+local plopts = g.nvls_options.player.options
 
 local lilyPlayer = lilyPopup({
   enter = true,
   focusable = true,
   border = {
     text = { top = "[" .. vim.g.nvls_short .. ".mp3]" },
-    style = vim.g.nvls_options.player.options.border_style,
+    style = plopts.border_style,
   },
     position = {
-    row = vim.g.nvls_options.player.options.row,
-    col = vim.g.nvls_options.player.options.col,
+    row = plopts.row,
+    col = plopts.col,
   },
   size = {
-    width = vim.g.nvls_options.player.options.width,
-    height = vim.g.nvls_options.player.options.height,
+    width = plopts.width,
+    height = plopts.height,
   },
   buf_options = {
     modifiable = false,
     readonly = true,
   },
   win_options = {
-    winhighlight = "Normal:Normal,FloatBorder:Normal",
+    winhighlight = plopts.winhighlight,
   },
 })
 
@@ -31,49 +33,29 @@ vim.api.nvim_buf_call(lilyPlayer.bufnr, function()
     "--loop --config-dir=/tmp/ " .. vim.g.lilyAudioFile)
 end)
 
-local nrm = { noremap = true }
-local opt = vim.g.nvls_options.player
+local M = {}
 
-lilyPlayer:map('n', 
-  opt.mappings.quit,
-  function() lilyPlayer:unmount() end, nrm)
-lilyPlayer:map('t', 
-  opt.mappings.quit,
-  function() lilyPlayer:unmount() end, nrm)
-lilyPlayer:map('t', 
-  vim.g.nvls_options.lilypond.mappings.switch_buffers,
-  "<cmd>stopinsert<cr><C-w>w", nrm)
-lilyPlayer:map('n', 
-  vim.g.nvls_options.lilypond.mappings.switch_buffers,
-  "<cmd>stopinsert<cr><C-w>w", nrm)
-lilyPlayer:map('n', 
-  opt.mappings.backward,
-  "i<Left><cmd>stopinsert<cr>", nrm)
-lilyPlayer:map('n', 
-  opt.mappings.forward,
-  "i<Right><cmd>stopinsert<cr>", nrm)
-lilyPlayer:map('n',
-  opt.mappings.small_forward,
-  "i<S-Right><cmd>stopinsert<cr>", nrm)
-lilyPlayer:map('n', 
-  opt.mappings.small_backward,
-  "i<S-Left><cmd>stopinsert<cr>", nrm)
-lilyPlayer:map('n', 
-  opt.mappings.play_pause,
-  "ip<cmd>stopinsert<cr>", nrm)
-lilyPlayer:map('n', 
-  opt.mappings.halve_speed,
-  "i{<cmd>stopinsert<cr>", nrm)
-lilyPlayer:map('n', 
-  opt.mappings.double_speed,
-  "i}<cmd>stopinsert<cr>", nrm)
-lilyPlayer:map('n', 
-  opt.mappings.decrease_speed,
-  "i[<cmd>stopinsert<cr>", nrm)
-lilyPlayer:map('n', 
-  opt.mappings.increase_speed,
-  "i]<cmd>stopinsert<cr>", nrm)
-lilyPlayer:map('n', 
-  opt.mappings.loop,
-  "il<cmd>stopinsert<cr>", nrm)
-lilyPlayer:map('n', ':', "", nrm)
+local nrm = { noremap = true }
+local opt = g.nvls_options.player.mappings
+local lyopt = g.nvls_options.lilypond.mappings
+
+function M.map(key,cmd)
+  lilyPlayer:map('n', key, cmd, nrm)
+end
+
+M.map(opt.quit,             function() lilyPlayer:unmount() end)
+M.map(lyopt.switch_buffers, "<cmd>stopinsert<cr><C-w>w")
+M.map(opt.backward,         "i<Left><cmd>stopinsert<cr>")
+M.map(opt.forward,          "i<Right><cmd>stopinsert<cr>")
+M.map(opt.small_forward,    "i<S-Right><cmd>stopinsert<cr>")
+M.map(opt.small_backward,   "i<S-Left><cmd>stopinsert<cr>")
+M.map(opt.play_pause,       "ip<cmd>stopinsert<cr>")
+M.map(opt.halve_speed,      "i{<cmd>stopinsert<cr>")
+M.map(opt.double_speed,     "i}<cmd>stopinsert<cr>")
+M.map(opt.decrease_speed,   "i[<cmd>stopinsert<cr>")
+M.map(opt.increase_speed,   "i]<cmd>stopinsert<cr>")
+M.map(opt.loop,             "il<cmd>stopinsert<cr>")
+M.map(':',                  "")
+M.map('i',                  "")
+
+return M
