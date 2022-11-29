886210895e95c2ff32f54efaa59936a871708d7a
martineausimon <martineau.simon@gmail.com>
2022-11-29 22:47:49 +0100

    lua/nvls.lua: adding mpv_flags defaults to setup()

--- lua/nvls.lua
+++ lua/nvls.lua
@@ -54,7 +54,8 @@ local default = {
       width = "37",
       height = "1",
       border_style = "single",
-      winhighlight = "Normal:Normal,FloatBorder:Normal"
+      winhighlight = "Normal:Normal,FloatBorder:Normal",
+      mpv_flags = "--msg-level=cplayer=no,ffmpeg=no --loop --config-dir=/dev/null"
     },
   },
 }
