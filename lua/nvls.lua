0977bc8be5b2fbc477bf0f17b1c02b48a7af9896
martineausimon <martineau.simon@gmail.com>
2023-06-29 00:44:44 +0200

    Improve showDiagnostics()

--- lua/nvls.lua
+++ lua/nvls.lua
@@ -197,27 +197,34 @@ function M.viewer(file)
   end
 end
 
-function M.showDiagnostics(lines,errorfm,ctrl)
+function M.showDiagnostics(lines, errorfm, ctrl)
   vim.diagnostic.reset()
+  local file_diagnostics = {}
   local filtered_lines = {}
-  local diagnostics = {}
-  local ns = vim.api.nvim_create_namespace("lilypond-diagnostics")
+
   for _, line in pairs(lines) do
     local filename, row, col, message = string.match(line,'^([^%s].+):(%d+):(%d+): (.+)$')
     if filename then
-      local bfnr = vim.fn.bufnr(vim.fn.expand(filename))
       message = string.gsub(message, '^error: ', '')
-      table.insert(diagnostics, {
+      if not file_diagnostics[filename] then
+        file_diagnostics[filename] = {}
+      end
+      table.insert(file_diagnostics[filename], {
         severity = vim.diagnostic.severity.ERROR,
         message = message,
         lnum = tonumber(row) - 1,
         col = tonumber(col) -1,
       })
-      vim.diagnostic.set(ns, bfnr, diagnostics, {})
+      if filename ~= vim.fn.expand("%:p") then
+        table.insert(filtered_lines, line)
+      end
     end
-    if filename ~= vim.fn.expand("%:p") then
-      table.insert(filtered_lines, line)
-    end
+  end
+
+  local ns = vim.api.nvim_create_namespace("lilypond-diagnostics")
+  for filename, diagnostics in pairs(file_diagnostics) do
+    local bfnr = vim.fn.bufnr(vim.fn.expand(filename))
+    vim.diagnostic.set(ns, bfnr, diagnostics, {})
   end
 
   vim.fn.setqflist({}, " ", {
