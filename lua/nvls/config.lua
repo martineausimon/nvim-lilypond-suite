9515f7cdf96a7af62be303b4bc5576b17365afa4
martineausimon <martineau.simon@gmail.com>
2023-11-05 00:44:48 +0100

    refactor: Use vim.bo.filetype instead of arg for fileInfos()

--- lua/nvls/config.lua
+++ lua/nvls/config.lua
@@ -3,48 +3,63 @@ local nvls_options = require('nvls').get_nvls_options()
 local audio_format = nvls_options.player.options.audio_format
 local midi_synth = nvls_options.player.options.midi_synth
 
-local main_folder
-local main_file
-
 local M = {}
 
-function M.fileInfos(ft)
-  local file = {}
 
-  if ft == "tex" then
+function M.fileInfos()
+  local file = {}
+  local main_folder, main_file, lb_flags
+  local backend = nvls_options.lilypond.options.backend
+
+  if vim.bo.filetype == "tex" then
     main_folder = nvls_options.latex.options.main_folder
     main_file = nvls_options.latex.options.main_file
-  elseif ft == "lilypond" then
+    lb_flags = nvls_options.latex.options.lilypond_book_flags or ''
+    if backend then
+      backend = '--process "lilypond -dbackend=' .. backend .. '"'
+    end
+  elseif vim.bo.filetype == "texinfo" then
+    main_folder = nvls_options.texinfo.options.main_folder
+    main_file = nvls_options.texinfo.options.main_file
+    lb_flags = nvls_options.texinfo.options.lilypond_book_flags or ''
+    if backend then
+      backend = '--process "lilypond -dbackend=' .. backend .. '"'
+    end
+  elseif vim.bo.filetype == "lilypond" then
     main_folder = nvls_options.lilypond.options.main_folder
     main_file = nvls_options.lilypond.options.main_file
+    if backend then
+      backend = '-dbackend=' .. backend
+    end
   end
 
-  local main = Utils.shellescape(vim.fn.expand('%:p'))
+  if type(lb_flags) == "table" then
+    lb_flags = table.concat(lb_flags, " ")
+  end
+
+  local main = Utils.shellescape(vim.fn.expand('%:p'), true)
   local main_path = Utils.joinpath(vim.fn.expand(main_folder), main_file)
 
   if Utils.exists(main_path) then
-    main = Utils.shellescape(main_path)
+    main = Utils.shellescape(main_path, true)
   end
 
-  local name = main:gsub("%.(i?ly)$", ""):gsub("%.tex$", "")
-
-  local os = vim.loop.os_uname().sysname
-  if os == "Windows" then
-    file.name = name:match('.*\\([^\\]+)$')
-  else
-    file.name = name:match('.*/([^/]+)$'):gsub([[\]], "")
-  end
+  local name = Utils.remove_extension(main)
+  name = Utils.shellescape(name, false)
 
   if os == "Windows" or midi_synth == "timidity" then
     audio_format = "wav"
   end
 
-  file.pdf    = Utils.change_extension(main, "pdf")
-  file.audio  = Utils.change_extension(main, audio_format)
-  file.midi   = Utils.change_extension(main, "midi")
-  file.main   = main
-  file.folder = Utils.shellescape(vim.fn.expand(main_folder))
-  file.tmp    = Utils.joinpath(vim.fn.stdpath('cache'), 'nvls')
+  file.name     = Utils.remove_path(name)
+  file.pdf      = Utils.change_extension(main, "pdf")
+  file.audio    = Utils.change_extension(main, audio_format)
+  file.midi     = Utils.change_extension(main, "midi")
+  file.main     = main
+  file.folder   = Utils.shellescape(vim.fn.expand(main_folder), true)
+  file.tmp      = Utils.joinpath(vim.fn.stdpath('cache'), 'nvls')
+  file.backend  = backend or ''
+  file.lb_flags = lb_flags
   vim.fn.mkdir(file.tmp, 'p')
 
   return file
