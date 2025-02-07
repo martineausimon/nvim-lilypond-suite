0cde5304b0c87f44194fcf5fcb8422d3312ba8be
martineausimon <martineau.simon@gmail.com>
2025-02-07 13:43:34 +0100

    chore(viewer): Improve pdf_viewer option

--- lua/nvls/viewer.lua
+++ lua/nvls/viewer.lua
@@ -1,7 +1,26 @@
 local Utils = require('nvls.utils')
 local nvls_options = require('nvls').get_nvls_options()
+local os = vim.loop.os_uname().sysname
+local ft = vim.bo.ft
 
 local M = {}
+
+local function viewer_prg()
+
+  local os_cmd = {
+    ["Darwin"] = "open",
+    ["Linux"] = "xdg-open",
+    ["Windows"] = "start"
+  }
+
+  local config_cmd = {
+    ["lilypond"] = nvls_options.lilypond.options.pdf_viewer,
+    ["tex"] = nvls_options.latex.options.pdf_viewer,
+    ["texinfo"] = nvls_options.texinfo.options.pdf_viewer
+  }
+
+  return config_cmd[ft] or os_cmd[os]
+end
 
 function M.open(file, name)
   name = name or nil
@@ -10,21 +29,9 @@ function M.open(file, name)
     do return end
   end
 
-  local os = vim.loop.os_uname().sysname
 
-  local os_commands = {
-      ["Darwin"] = "open",
-      ["Linux"] = "xdg-open",
-      ["Windows"] = "start"
-  }
-
-  local cmd = os_commands[os]
-
-  if nvls_options.lilypond.options.pdf_viewer ~= nil then
-    cmd = nvls_options.lilypond.options.pdf_viewer
-    vim.fn.jobstart(string.format('%s %s', cmd, file))
-  elseif cmd then
-    vim.fn.jobstart(string.format('%s %s', cmd, file))
+  if viewer_prg() then
+    vim.fn.jobstart(string.format('%s %s', viewer_prg(), file))
   else
     Utils.message(string.format("Unsupported operating system : %s", os), "ERROR")
   end
